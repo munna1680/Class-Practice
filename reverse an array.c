211fbf853f0098cf79fd62920278d7f27ff0792a
Munna <shakibhassanmunna777@gmail.com>
2025-01-26 19:26:03 +0600

    own practice

---
+++ reverse an array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    int i=0,j=n-1;
+
+    for(int i=0;i<n;i++)
+    {
+            if(i<j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j--;
+        
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    
+}
